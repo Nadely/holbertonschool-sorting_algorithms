a3b1dab7995fa505880e1aa8bbc9be4a5005282e
ER1-53 <cremey.erwan@gmail.com>
2023-09-18 22:36:48 +0200

    creat funct slection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,8 +10,27 @@
 
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size == 0)
+	size_t i, j, min_index;
+	int tmp;
+
+	if (array == NULL || size < 2)
 		return;
 
+	for (j = 0; j < size - 1; j++)
+	{
+		min_index = j;
+		for (i = j + 1; i < size; i++)
+		{
+			if (array[i] < array[min_index])
+				min_index = i;
+		}
 
+			if (min_index != i)
+			{
+				tmp = array[j];
+				array[j] = array[min_index];
+				array[min_index] = tmp;
+				print_array(array, size);
+			}
+	}
 }
