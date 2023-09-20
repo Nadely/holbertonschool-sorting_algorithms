ac8267b16ce9953c55e143a744f2a72c5773b916
Nadely <6890@holbertonstudents.com>
2023-09-20 14:43:48 +0200

    delete space in shell_sort function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -27,24 +27,24 @@ void knuth(int *sequence, size_t size)
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t i, j;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
 }
 
 /**
