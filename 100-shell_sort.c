8c53adc3de23f8e4441a6c634f64805b26f59a7f
Nadely <6890@holbertonstudents.com>
2023-09-20 16:49:42 +0200

    correction description in shell_sort function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- * shell_sort - Tri un tableau d'entiers en utilisant l'algorithme de tri de Shell.
- * @array: Le tableau à trier.
- * @size: La taille du tableau.
+ * shell_sort - Sorts an array of integers in ascending order
+ *using the Shell sort algorithm with Knuth sequence.
+ * @array: Pointer to the array to be sorted
+ * @size: Size of the array
  */
 
 void shell_sort(int *array, size_t size)
@@ -14,8 +15,8 @@ void shell_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	while (jump < size/3)
-		jump = jump *3 + 1;
+	while (jump < size / 3)
+		jump = jump * 3 + 1;
 
 	for (; jump > 0; jump = (jump - 1) / 3)
 	{
@@ -23,14 +24,13 @@ void shell_sort(int *array, size_t size)
 		{
 			tmp = array[i];
 			for (j = i; j >= jump && array[j - jump] > tmp;
-					j -= jump)
+				 j -= jump)
 			{
 				if (array[j] != array[j - jump])
 					array[j] = array[j - jump];
 			}
 			if (array[j] != tmp)
 				array[j] = tmp;
-
 		}
 		print_array(array, size);
 	}
