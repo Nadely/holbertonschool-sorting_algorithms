3bc09a03108a48b07ea983a84b11a7268afaae42
ER1-53 <cremey.erwan@gmail.com>
2023-09-21 12:17:35 +0200

    creat 102-counting & 102-O & sort.h

---
+++ 102-counting_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/**
+ * counting_sort -  function that sorts an array of integers in ascending
+ * order using the Counting sort algorithm
+ *
+ * @array: Pointer to the array to be sorted
+ * @size: Size of the array
+ *
+ * Return: nothing
+ */
+
+void counting_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	
+}
